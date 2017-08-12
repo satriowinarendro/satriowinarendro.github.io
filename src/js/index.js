a949dbaa54d9a91d2ef2d4dc3383c82cdae2cba8
satrioWinarendro <satrio.winarendro@gmail.com>
2017-08-12 16:00:34 +0700

    add section backgrounds, insert parallaxjs, set section bg to parallax

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,10 @@ $ = jQuery = require('jquery');
 global.Tether = require('tether');
 global.Popper = require('popper.js');
 require('bootstrap');
+require('../../dist/js/parallax.min.js');
 
-
+$(function(){
+    $('.parallax-skillset').parallax({imageSrc: 'dist/img/skillset-bg.jpg'});
+    $('.parallax-home').parallax({imageSrc: 'dist/img/main-banner.jpg'});
+    $('.parallax-contact').parallax({imageSrc: 'dist/img/contact-bg.jpg'});
+});
