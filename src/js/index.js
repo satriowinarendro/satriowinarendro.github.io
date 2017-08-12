5d47e88090f5050c78856db5e7eb8c8cd66b2bac
satrioWinarendro <satrio.winarendro@gmail.com>
2017-08-12 11:32:09 +0700

    add bootstrap.min.css to dist, install popper.js bootstrap requirement

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@ var $ = require('jquery');
 window.$ = $;
 window.jQuery = window.$;
 global.Tether = require('tether');
+global.Popper = require('popper.js');
 require('bootstrap');
 
 console.log("test test test");
